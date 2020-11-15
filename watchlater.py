db003a0a4f153c5c65a9929110b5910f632c34e6
brandon0824 <446038117@qq.com>
2020-11-15 08:55:42 +0800

    update

--- watchlater.py
+++ watchlater.py
@@ -50,7 +50,7 @@ def getRSS():
             listHourLag.append(hourLag)
             listMinsLag.append(minsLag)
             listAVNum.append(i.link)
-        if time.localtime()[3] >= 0 and time.localtime()[3] <= 8 and publishHour >= -24 and publishHour <= -16:
+        if time.localtime()[3] >= 0 and time.localtime()[3] <= 8 and publishHour >= 16 and publishHour <= 24:
             listHourLag.append(hourLag)
             listMinsLag.append(minsLag)
             listAVNum.append(i.link)
